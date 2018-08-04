50d29a6d8dd539bc245a8b92007b8960d68694ec
Troy Neubauer <troyneubauer@gmail.com>
2018-08-04 15:29:48 -0400

    Created App android studio project

---
+++ App/app/src/main/java/com/jt/scouting/JTActivity.java
@@ -0,0 +1,12 @@
+package com.jt.scouting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JTActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
