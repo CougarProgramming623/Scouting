b8c3c1992da33264bc389945d8ccc04f34c5f71a
Troy Neubauer <troyneubauer@gmail.com>
2018-10-28 15:49:17 -0400

    Attempted to add dependencies to Assigner Project

--- Scouting App Assigner/src/main/java/com/jt/saa/Main.java
+++ Scouting App Assigner/src/main/java/com/jt/saa/Main.java
@@ -1,6 +1,8 @@
 package com.jt.saa;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
@@ -14,6 +16,8 @@ public class Main {
 		File excelFile = chooser.getSelectedFile();
 		int numDevices = Integer.parseInt(JOptionPane.showInputDialog(chooser, "Enter the number of devices to export for", "Enter Number of Devices", JOptionPane.INFORMATION_MESSAGE));
 		
+		int matchC = -1;
+		List<Integer> redTeams = new ArrayList<Integer>(), blueTeams = new ArrayList<Integer>();
+		
 	}
-
 }
